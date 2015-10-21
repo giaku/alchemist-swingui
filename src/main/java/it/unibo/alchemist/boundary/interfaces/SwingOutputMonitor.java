c9f47cce18e773c75b0463fb64c84e2c3d909d52
Gianluca Turin <turin.gianluca@educ.di.unito.it>
2015-10-21 18:00:49 +0200

    added interface SwingOutputMonitor

---
+++ src/main/java/it/unibo/alchemist/boundary/interfaces/SwingOutputMonitor.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package it.unibo.alchemist.boundary.interfaces;
+
+/**
+ * @author Gianluca Turin
+ *
+ * @param T
+ */
+public interface SwingOutputMonitor<T> extends GraphicalOutputMonitor<T> {
+	
+}
