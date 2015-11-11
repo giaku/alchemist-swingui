cd56223883057d788c4c6402357fa7a957985cb5
Gianluca Turin <turin.gianluca@educ.di.unito.it>
2015-11-11 17:36:35 +0100

    added features to the RecordingMonitor

--- src/main/java/it/unibo/alchemist/boundary/monitors/RecordingMonitor.java
+++ src/main/java/it/unibo/alchemist/boundary/monitors/RecordingMonitor.java
@@ -10,6 +10,8 @@
 package it.unibo.alchemist.boundary.monitors;
 
 
+import it.unibo.alchemist.boundary.gui.effects.DrawShape;
+import it.unibo.alchemist.boundary.gui.effects.Effect;
 import it.unibo.alchemist.boundary.interfaces.GraphicalOutputMonitor;
 import it.unibo.alchemist.model.implementations.times.DoubleTime;
 import it.unibo.alchemist.model.interfaces.IEnvironment;
@@ -17,19 +19,27 @@ import it.unibo.alchemist.model.interfaces.IReaction;
 import it.unibo.alchemist.model.interfaces.ITime;
 import it.unibo.alchemist.utils.L;
 
+import java.awt.Point;
+import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.Semaphore;
 
 import org.jfree.graphics2d.svg.SVGGraphics2D;
 import org.apache.commons.math3.util.FastMath;
+import org.danilopianini.io.FileUtilities;
+import org.danilopianini.lang.RangedInteger;
 import org.danilopianini.view.ExportForGUI;
 
 
@@ -44,28 +54,57 @@ public class RecordingMonitor<T> extends EnvironmentInspector<T> {
 	
 	
 	/**
+	 * The source reactivity.
 	 * 
+	 * @author Gianluca Turin
+	 *
 	 */
+	protected enum ReactivityMode {
+		REALTIME, MAX
+	}
+	
 	private static final long serialVersionUID = 1L;
 	private final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS", Locale.getDefault());
 	private Generic2DDisplay<T> source;
 	private final Semaphore mutex;
 	private String fpCache;
-	private final String defaultFilePath = System.getProperty("user.home") + System.getProperty("file.separator") + sdf.format(new Date()) + "-alchemist_screenshot";
+	private String efCache;
+	private final String defaultFilePath = System.getProperty("user.home") + System.getProperty("file.separator") + sdf.format(new Date()) + "-alchemist_screenshots";
+	private final String defaultEffectsFile = System.getProperty("user.home") + System.getProperty("file.separator") + "???";
 	private PrintStream writer;
+	private int screenCounter = 0;
+	private final List<Effect> defEffects = new ArrayList<Effect>(Collections.singletonList(new DrawShape()));
 	private SVGGraphics2D svgGraphicator;
 	private long lastStep = Long.MIN_VALUE;
 	private double lastUpdate = Long.MIN_VALUE;
-	
 	@SuppressWarnings("rawtypes")
 	private static final Class< ? extends GraphicalOutputMonitor> DEFAULT_MONITOR_CLASS = Generic2DDisplay.class;
 	private static final String DEFAULT_MONITOR_PACKAGE = "it.unibo.alchemist.boundary.monitors.";
+	private static final int MIN_WIDTH = 800;
+	private static final int DEF_WIDTH = 1000;
+	private static final int MAX_WIDTH = 2000;
+	private static final int MIN_HEIGHT = 800;
+	private static final int DEF_HEIGHT = 1000;
+	private static final int MAX_HEIGHT = 2000;
+	private static final int MAX_ZOOM = 255;
 	
-	@ExportForGUI(nameToExport = "Capture after initialization")
-	private boolean startingScreenshot = true;
+	@ExportForGUI(nameToExport = "Zoom rate (leave 0 for optimal)")
+	private RangedInteger zoom = new RangedInteger(0, MAX_ZOOM, 0);
+	@ExportForGUI(nameToExport = "Reactivity")
+	private ReactivityMode reactMode = ReactivityMode.MAX;
+	@ExportForGUI(nameToExport = "Width")
+	private RangedInteger width = new RangedInteger(MIN_WIDTH, MAX_WIDTH, DEF_WIDTH);
+	@ExportForGUI(nameToExport = "Height")
+	private RangedInteger height = new RangedInteger(MIN_HEIGHT, MAX_HEIGHT, DEF_HEIGHT);
+	@ExportForGUI(nameToExport = "Draw links")
+	private boolean drawLinks = false;
+	@ExportForGUI(nameToExport = "Effects file")
+	private String effectsFile = defaultEffectsFile;
+	@ExportForGUI(nameToExport = "POV dx (%)")
+	private RangedInteger povX = new RangedInteger(-100, 100, 0);
+	@ExportForGUI(nameToExport = "POV dy (%)")
+	private RangedInteger povY = new RangedInteger(-100, 100, 0);
 	
-	@ExportForGUI(nameToExport = "Capture when finished")
-	private boolean endingScreenshot = true;
 	
 	/**
 	 * RecordingMonitor<T> empty constructor.
@@ -94,8 +133,7 @@ public class RecordingMonitor<T> extends EnvironmentInspector<T> {
 			}
 		}
 		try {
-			source = (Generic2DDisplay<T>) monitorClass.getConstructor().newInstance();
-			
+			source = (Generic2DDisplay<T>) monitorClass.getConstructor().newInstance();	
 		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
 			L.error(e);
 		}
@@ -103,31 +141,40 @@ public class RecordingMonitor<T> extends EnvironmentInspector<T> {
 
 	@Override
 	public void finished(final IEnvironment<T> env, final ITime time, final long step) {
-		if (endingScreenshot) {
-			saveScreenshot(env, null, time, step);
-		}
-		
+		saveScreenshot(env, null, time, step);
 		source.finished(env, time, step);
 	}
 
+	/**
+	 * @return current effects stack file.
+	 */
+	public String getEffectsFile() {
+		return effectsFile;
+	}
+
 	@Override
-	public void initialized(final IEnvironment<T> env) {
-		
-		//TODO aggiungere caricamento via reflection di source
-				
+	public void initialized(final IEnvironment<T> env) {		
 		createMonitor(env);
-		
+		source.setVisible(true);
+		source.setEnabled(true);
+		source.setRealTime(reactMode.equals(ReactivityMode.REALTIME));
+		source.setSize(width.getVal(), height.getVal());
 		source.initialized(env);
-		
-		source.setRealTime(true);
-		
-		source.setSize(2000, 2000);
-		
-		
-		
-		if (startingScreenshot) {
-			saveScreenshot(env, null, new DoubleTime(), 0);
+		for (MouseListener listener : source.getMouseListeners()) {
+			source.removeMouseListener(listener);
 		}
+		//avoid nearest node circle
+		source.setDist(0, 0);
+		
+		saveScreenshot(env, null, new DoubleTime(), 0);
+	}
+	
+	/**
+	 * Set the effects file path.
+	 * @param ef new file path
+	 */
+	public void setEffectsFile(final String ef) {
+		effectsFile = ef;
 	}
 	
 	@Override
@@ -135,9 +182,6 @@ public class RecordingMonitor<T> extends EnvironmentInspector<T> {
 		mutex.acquireUninterruptibly();
 		final double sample = getInterval().getVal() * FastMath.pow(10, getIntervalOrderOfMagnitude().getVal());
 		final boolean log = getMode().equals(Mode.TIME) ?  time.toDouble() - lastUpdate >= sample : step - lastStep >= sample;
-		if (source != null) {
-			source.stepDone(env, r, time, step);
-		}
 		if (log) {
 			lastUpdate = time.toDouble();
 			lastStep = step;
@@ -153,28 +197,55 @@ public class RecordingMonitor<T> extends EnvironmentInspector<T> {
 	 * @param time the current time of the simulation that could be added to the file name
 	 * @param step the current step of the simulation that could be added to the file name
 	 */
+	@SuppressWarnings("unchecked")
 	private void saveScreenshot(final IEnvironment<T> env, final IReaction<T> r, final ITime time, final long step) {
-		
-		if (System.identityHashCode(fpCache) != System.identityHashCode(getFilePath())) {
-			fpCache = getFilePath();
-		}
-		lastStep = step;
-		lastUpdate = time.toDouble();
-		String currentStep = isLoggingStep() ? getSeparator() + step : "";
-		String currentTime = isLoggingTime() ? getSeparator() + time : "";			
+		if (source != null) {
+			source.stepDone(env, r, time, step);
+			if (System.identityHashCode(fpCache) != System.identityHashCode(getFilePath())) {
+				fpCache = getFilePath();
+			}
 			
-		try {
-			writer = new PrintStream(new File(fpCache + currentStep + currentTime + ".svg"), StandardCharsets.UTF_8.name());
-		} catch (FileNotFoundException | UnsupportedEncodingException e) {
-			L.error(e);
+			if (System.identityHashCode(efCache) != System.identityHashCode(getEffectsFile())) {
+				efCache = getEffectsFile();
+				List<Effect> effects = null;
+				try {		
+					effects = (List<Effect>) FileUtilities.fileToObject(getEffectsFile());
+				} catch (IOException | ClassNotFoundException e1) {
+					effects = defEffects;
+				} finally {
+					source.setEffectStack(effects);
+					source.revalidate();
+				}
+			}
+			
+			final int zoomVal = zoom.getVal();
+			if (zoomVal == 0) {
+				source.getWormhole().setOptimalZoomRate();
+			} else {
+				source.getWormhole().zoomOnPoint(new Point(0, height.getVal()), zoomVal);
+			}
+			source.revalidate();
+			
+			source.getWormhole().setViewPosition(new Point(width.getVal() * povX.getVal() / 100, (height.getVal() * povY.getVal() / 100) + height.getVal()));
+			
+			lastStep = step;
+			lastUpdate = time.toDouble();
+			String currentStep = isLoggingStep() ? getSeparator() + step : "";
+			String currentTime = isLoggingTime() ? getSeparator() + time : "";			
+				
+			try {
+				new File(fpCache).mkdirs();
+				writer = new PrintStream(new File(fpCache + System.getProperty("file.separator") + screenCounter++ + currentStep + currentTime + ".svg"), StandardCharsets.UTF_8.name());
+			} catch (FileNotFoundException | UnsupportedEncodingException e) {
+				L.error(e);
+			}
+			
+			svgGraphicator = new SVGGraphics2D(width.getVal(), height.getVal());
+			source.setDrawLinks(drawLinks);
+			source.paintComponent(svgGraphicator);
+			writer.print(svgGraphicator.getSVGDocument());
+			writer.close();
 		}
-		
-		svgGraphicator = new SVGGraphics2D(/*source.getWidth()*/2000, /*source.getHeight()*/2000);
-		source.setVisible(true);
-		source.setEnabled(true);
-		source.paintComponent(svgGraphicator);
-		writer.print(svgGraphicator.getSVGDocument());
-		writer.close();
 		
 	}
 	
@@ -185,5 +256,4 @@ public class RecordingMonitor<T> extends EnvironmentInspector<T> {
 		 */
 		return new double[0];
 	}
-	
 }
